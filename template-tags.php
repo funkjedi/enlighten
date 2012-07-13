e77b52c4c14205cac3ffa1bf7f1edefa969fc4d3
Tim Robertson <funkjedi@gmail.com>
2012-07-13 16:22:25 -0400

    Adding new loop tags and mobile detection tag

--- template-tags.php
+++ template-tags.php
@@ -1,5 +1,13 @@
 <?php
 
+function mdetect($method) {
+	static $mdetect;
+	if (!$mdetect) {
+		require_once dirname(__FILE__) . '/vendor/mdetect.php';
+		$mdetect = new uagent_info();
+	}
+	return $mdetect->$method();
+}
 
 function wp_enqueue_less($stylesheet) {
 	if (!class_exists('lessc')) {
