ca82edb631c204fb2d149f02357be184c697a565
Tim Robertson <funkjedi@gmail.com>
2012-07-31 14:38:03 -0400

    Added option to use | in place of &

--- shortcodes.php
+++ shortcodes.php
@@ -38,7 +38,8 @@ add_shortcode('display_posts','shortcode__display_posts');
 function shortcode__display_posts($atts) {
 	extract(shortcode_atts(array('where' => '', 'using' => ''), $atts));
 	ob_start();
-	display_posts(wp_parse_args(html_entity_decode($where)), 'get_template_part', array($using));
+	$where = strtr(html_entity_decode($where), '|', '&');
+	display_posts(wp_parse_args($where), 'get_template_part', array($using));
 	return ob_get_clean();
 }
 
@@ -46,7 +47,8 @@ add_shortcode('loop','shortcode__loop');
 function shortcode__loop($atts) {
 	extract(shortcode_atts(array('where' => '', 'tpl' => ''), $atts));
 	ob_start();
-	the_loop(wp_parse_args(html_entity_decode($where)));
+	$where = strtr(html_entity_decode($where), '|', '&');
+	the_loop(wp_parse_args($where));
 	get_template_part($tpl);
 	return ob_get_clean();
 }
